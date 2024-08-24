41129cf2d95bfeb97c8f8271566b39fd72a9e2ae
Victor Berdugo <vberdugo@car8s5.42barcelona.com>
2024-08-24 15:59:54 +0200

    1st commit

--- push_swap.h
+++ push_swap.h
@@ -6,8 +6,27 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 14:18:49 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/08/24 14:18:51 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/08/24 15:38:57 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <stdbool.h>
+# include <limits.h>
+# include "/libft/libft.h"
+
+typedef struct s_stack
+{
+	int				nbr;
+	int				index;
+	int				push_cost;
+	bool			above_median;
+	bool			cheapest;
+	struct s_stack	*target;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
+
+#endif
